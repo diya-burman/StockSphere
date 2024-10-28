63bc7c4610712bc865bb689f923bad11cfdd2768
Diya Burman <diyaburman60@gmail.com>
2024-10-29 02:25:22 +0530

    changes

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,13 +1,32 @@
 "use client"
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsSidebarCollapsed } from '@/state';
 import { Menu } from 'lucide-react';
 import React from 'react'
 
 const Sidebar = () => {
-    return <div>
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed);
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72:w-64"
+  } bg-white transition-all duration-300 overflow-hidde h-full shadow-md z-40`;
+
+    return (
+    <div className={sidebarClassNames}>
         {/* TOP LOGO */}
-        <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+          isSidebarCollapsed ? "px-5" : "px-8"}`}>
+
             <div>logo</div>
-            <h1 className="font-extrabold text-2xl">StockSphere</h1>
+            <h1 className={`${isSidebarCollapsed ? "hidden" : "block" 
+            }font-extrabold text-2xl`}>StockSphere</h1>
         
         <button className="md;hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() => {}}>
             <Menu className="w-4 h-4" />
@@ -23,7 +42,9 @@ const Sidebar = () => {
       <div>
         <p className="text-center text-xs text-gray-500">&copy; 2024 StockSphere</p>
       </div>
-    </div>;
+    <div/>
+  </div>
+  );
 };
 
 export default Sidebar;
