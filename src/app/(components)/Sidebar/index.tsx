3955c1bd7d06f3db69970e2ecdd102d8bb52ba49
Diya Burman <diyaburman60@gmail.com>
2024-10-29 02:59:05 +0530

    intial Changes

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,8 +1,41 @@
 "use client"
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import { setIsSidebarCollapsed } from '@/state';
-import { Menu } from 'lucide-react';
+import { Archive, CircleDollarSign, Layout, Link, LucideIcon, Menu, SlidersHorizontal, User, Clipboard } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 import React from 'react'
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed
+}: SidebarLinkProps) => {
+   const pathname = usePathname();
+   const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+   
+   return (
+    <Link href={href}>
+      <div className={`cursor-pointer flex items-center ${
+        isCollapsed ? "justify-center py-4" : 'justify-start px-8 py-4'}
+        hover: text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? "bg-blue-200 text-white" : ""}
+      }`}>
+        <Icon className="w=6 h-6 !text-gray-700" />
+        <span className={`${isCollapsed ? "hidden" : "block"
+        } font-medium text-gray-700`}>
+          {label}
+        </span>
+      </div>
+    </Link>
+   )
+}
 
 const Sidebar = () => {
 
@@ -35,7 +68,41 @@ const Sidebar = () => {
 
       {/* LINKS */}
       <div className="flex-grow mt-8">
-        {/* links here */}
+        <SidebarLink 
+        href= "/dashboard" 
+        icon={Layout} 
+        label="Dashboard" 
+        isCollapsed={isSidebarCollapsed} />
+
+        <SidebarLink 
+        href= "/inventory" 
+        icon={Archive} 
+        label="Inventory" 
+        isCollapsed={isSidebarCollapsed} />
+
+        <SidebarLink 
+        href= "/products" 
+        icon={Clipboard} 
+        label="Products" 
+        isCollapsed={isSidebarCollapsed} />
+
+        <SidebarLink 
+        href= "/users" 
+        icon={User} 
+        label="Users" 
+        isCollapsed={isSidebarCollapsed} />
+
+        <SidebarLink 
+        href= "/settings" 
+        icon={SlidersHorizontal} 
+        label="Settings" 
+        isCollapsed={isSidebarCollapsed} />
+
+        <SidebarLink 
+        href= "/expenses" 
+        icon={CircleDollarSign} 
+        label="Expenses" 
+        isCollapsed={isSidebarCollapsed} />
       </div>
 
       {/* FOOTER */}
